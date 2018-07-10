cea0755014c718a3905dd38ea519be30d692c9e4
nikhil reddy yampalla <nikhily8686@live.com>
2018-07-10 14:10:22 -0400

    Add project files.

---
+++ WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=LAPTOP-B898CURM;Initial Catalog=nikhil;Integrated Security=True"))
+            {
+                SqlDataAdapter sde = new SqlDataAdapter("Select * from persons", con);
+                DataSet ds = new DataSet();
+                sde.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
